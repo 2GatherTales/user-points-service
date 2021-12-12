d6b84e8d43e25626cca4b776f2a6ffc63d7a5f7e
Joao Rocha <joaogsrocha@gmail.com>
2021-12-12 09:46:15 +0000

    [Add] health endpoint

--- src/main/java/com/joaogsrocha/_SpringREST/controller/UserPointsController.java
+++ src/main/java/com/joaogsrocha/_SpringREST/controller/UserPointsController.java
@@ -8,11 +8,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 @RestController
 @RequestMapping("api/userpoints")
 public class UserPointsController {
     @Autowired
     private GenericService<UserPoints> userPointsRepository;
+
+    public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+
+    public static String now() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+        return sdf.format(cal.getTime());
+    }
+
+    @RequestMapping(value = "health",
+            method = RequestMethod.GET,
+            produces =  {MimeTypeUtils.APPLICATION_JSON_VALUE},
+            headers = "Accept=application/json")
+    public ResponseEntity<String> health() {
+        try {
+            String response = "OK   " + now();
+            return new ResponseEntity<String>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
     @RequestMapping(value = "findall",
             method = RequestMethod.GET,
